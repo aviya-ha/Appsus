aaf2b077d3da4d8e2b7ffa78f5b0f05104fa4646
aviya-ha <aviyaharel6@gmail.com>
2024-03-14 19:54:49 +0200

    Add css to mail index

--- cmps/AppNav.jsx
+++ cmps/AppNav.jsx
@@ -2,7 +2,6 @@ const { Link, NavLink } = ReactRouterDOM
 
 
 export function AppNav(){
-    <i class="fa-solid fa-note-sticky" style="color: #ffae3d;"></i>
     return<nav className="app-nav">
              <NavLink to="/" className="fa-solid fa-house" style={{color : '#69c96b'}}></NavLink>
             <NavLink to="/about" className="fa-regular fa-user" style={{color : '#69c96b'}}></NavLink>
