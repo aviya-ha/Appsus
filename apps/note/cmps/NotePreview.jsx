783d5c9bf0af214a8bc68f4131c526df04f09177
meni-kr <meni.kramer@gmail.com>
2024-03-14 13:01:47 +0200

    Add support background note color

--- apps/note/cmps/NotePreview.jsx
+++ apps/note/cmps/NotePreview.jsx
@@ -1,11 +1,49 @@
+const { useState} = React
+
+import { noteService } from "../services/note.service.js"
+
+import { ColorInput } from "./dinamic/ColorInput.jsx"
+
+
+// const { Link, useSearchParams } = ReactRouterDOM
 
 
 
-export function NotePreview({notes,onRemoveNote,onEditNote}){
-console.log('notes:', notes)
+export function NotePreview({ notes, onRemoveNote, onEditNote }) {
 
-    return <React.Fragment>
-        {
+	const [note, setNote] = useState(null)
+
+	// const [searchParams, setSearchParams] = useSearchParams()
+	// const { carId } = useParams()
+	// const isShowColorRef = useRef()
+
+	function loadNote(noteId) {
+
+		noteService.getById(noteId)
+			.then((note) => {
+				setNote(prevNotes =>({...prevNotes,...note}))
+			})
+	}
+
+	function onChangeStyle(style, note) {
+		note.style = ({ ...style })
+		noteService.save(note)
+			.then(savedNote => {
+				console.log('Note saved!!');
+				loadNote(savedNote.id)
+				// showSuccessMsg('Note saved successfully')
+			})
+			.catch(err => {
+				console.log('Had issues saving note', err)
+				// showErrorMsg('could not save note')
+			})
+		
+	}
+
+	
+
+	return <React.Fragment>
+		{
 			notes.map(note => <li style={note.style} className="card-note-container" key={note.id}>
 				<p>{note.info.title}</p>
 				<p>{note.info.txt}</p>
@@ -14,7 +52,12 @@ console.log('notes:', notes)
 					<button className="btn-edit-note" onClick={() => onEditNote(note.id)}>Edit</button>
 					<button className="btn-edit-note" onClick={() => onEditNoteColor(note)}>color</button>
 				</div>
+				{
+					<ColorInput
+						onChangeStyle={({style}) => onChangeStyle(style, note)} />
+				}
+
 			</li>)
 		}
-    </React.Fragment>
+	</React.Fragment>
 }
