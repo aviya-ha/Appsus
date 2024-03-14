e50feed7f651362e8cd6ac44daccfb3c732332de
aviya-ha <aviyaharel6@gmail.com>
2024-03-14 15:03:58 +0200

    Add full compose mail

--- apps/mail/cmps/ComposeMail.jsx
+++ apps/mail/cmps/ComposeMail.jsx
@@ -1,44 +1,68 @@
+const { useState, useEffect } = React
 
-export function ComposeMail({setIsComposeMail}) {
 
-    function showModal(){
-        console.log('hay');
+import { mailService } from "../services/mail.service.js"
+
+export function ComposeMail({ setIsComposeMail, saveMail, newMail,}) {
+	const [newMailToSave, setNewMailToSave] = useState(newMail)
+
+    
+    // useEffect(() => {
+	// 	setNewMail(newMailToSave)
+	// }, [newMailToSave])
+
+	function onSaveMail(ev) {
+        console.log('ev:', ev)
+		ev.preventDefault()
+        showModal()
+        console.log('newMailToSave:', newMailToSave)
+		saveMail(newMailToSave)
+        
+	}
+
+function handleChange({ target }){
+    let { value, name: field } = target
+    setNewMailToSave((prevNewMail) => ({ ...prevNewMail, [field]: value }))
+
+}
+
+    function showModal() {
         setIsComposeMail(false)
     }
 
     return <section className="mail-compose-container">
         <header className="mail-compose-header">
-            <h1>New Message</h1> <button className="fa-solid fa-xmark" onClick={showModal}></button>
+            <h1>New Message</h1> <button className="fa-solid fa-xmark"></button>
         </header>
-        <main className="mail-compose-main">
+        <form className="mail-compose-main" onSubmit={onSaveMail}>
             <input className="mail-compose-to"
                 type="text"
                 id="to"
                 name="to"
-                // value={filterByToEdit.search}
-                // onChange={onFilter}
+                value={newMailToSave.to}
+                onChange={handleChange}
                 placeholder="To" />
 
             <input className="mail-compose-subject"
                 type="text"
                 id="subject"
                 name="subject"
-                // value={filterByToEdit.search}
-                // onChange={onFilter}
+                value={newMailToSave.subject}
+                onChange={handleChange}
                 placeholder="Subject" />
 
             <input className="mail-compose-body"
                 type="text"
                 id="body"
                 name="body"
-                // value={filterByToEdit.search}
-                // onChange={onFilter}
-                // placeholder="Subject" 
-                />
-        </main>
+            value={newMailToSave.body}
+            onChange={handleChange}
+            // placeholder="Subject" 
+            />
+            <button className="btn btn-Send">Send</button>
+        </form>
 
         <footer className="mail-compose-footer">
-<button className="btn btn-Send">Send</button>
         </footer>
 
 
