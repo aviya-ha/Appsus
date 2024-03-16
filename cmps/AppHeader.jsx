a09bc374c3bbe1a9bac4a385623e72b9a525ecb6
meni-kr <meni.kramer@gmail.com>
2024-03-16 23:13:22 +0200

    Add style to about page

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -8,11 +8,11 @@ export function AppHeader() {
     const noteImg = "https://play-lh.googleusercontent.com/9bJoeaPbGTB8Tz_h4N-p-6ReRd8vSS-frZb2tmJulaGIoTKElKj3zpmcFJvnS96ANZP5=w240-h480-rw"
     return <header className="app-header">
         <Link to="/">
-            <h3>MISS!</h3>
+            <h3 className="app-header-logo fa-solid fa-horse-head"></h3>
         </Link>
         <nav className="app-header-nav">
-            <NavLink to="/" className="fa-solid fa-house" style={{color : '#69c96b'}}></NavLink>
-            <NavLink to="/about" className="fa-regular fa-user" style={{color : '#69c96b'}}></NavLink>
+            <NavLink to="/" className="fa-solid fa-house" style={{color : '#f7f89c'}}></NavLink>
+            <NavLink to="/about" className="fa-regular fa-user" style={{color : '#f7f89c'}}></NavLink>
             <NavLink to="/mail" > <img className="mail-icon" src={mailImg} alt="gmail" /></NavLink>
             <NavLink to="/note"> <img className="note-icon" src={noteImg} alt="note" /></NavLink>
         </nav>
--- views/About.jsx
+++ views/About.jsx
@@ -1,8 +1,13 @@
 import { AppHeader } from "../cmps/AppHeader.jsx"
 
 export function About() {
-    return <section className="about">
+    return <section className="about-container">
         <AppHeader />
-        <h1>About Page</h1>
+        <article className="about-article-container">
+           <h1>Somethings About us.</h1>
+        <h2>we are aviya and meni!</h2> 
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, perferendis. Enim minima non sit eius pariatur obcaecati incidunt ea est?</p>
+        </article>
+        
     </section>
 }
